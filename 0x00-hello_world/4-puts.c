dff7396a850dd0804f207e38838deead2ad552e5
FindingNemoo <carolmaina502@gmail.com>
2022-10-27 10:01:08 +0300

    new commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
+ HEAD
 #include<stdio.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
-=======
-#include <stdio.h>
-/**
- * main -entry point
- *
- * return : always 0 (success)
->>>>>>> b2c5aacdd0f3094cdaab89337299c1d0feefca4f
- */
-int main(void)
+*/
+ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 This project is an introduction to the C language.
-=======
-This is a C project.
->>>>>>> b2c5aacdd0f3094cdaab89337299c1d0feefca4f
